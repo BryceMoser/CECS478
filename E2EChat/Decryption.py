20292ab61f0503f0fdf48570399d91a281d9ebb6
FryedBryce <brycemoser@yahoo.com>
2018-10-11 16:21:30 -0700

    Loading PrivKey

--- E2EChat/Decryption.py
+++ E2EChat/Decryption.py
@@ -1,3 +1,12 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
+
+
+##retrieving private key
+with open("RSA2048 KeyPair\privkey.ppk", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend = default_backend()
+    )
