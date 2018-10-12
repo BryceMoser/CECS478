91497c88195e55014e2120a63354a9cfbcb32815
FryedBryce <brycemoser@yahoo.com>
2018-10-11 23:41:02 -0700

    removeExtra

--- E2EChat/Decryption.py
+++ E2EChat/Decryption.py
@@ -14,19 +14,3 @@ if '--d' in sys.argv:
             print('AES Ciphertext'+p['aes_ciphertxt'])
             print('HMAC tag'+p['htag'])
             print('Key path'+p['path'])
-        
-
-
-#Retrieving private key
-# with open("RSA2048 KeyPair\privkey.ppk", "rb") as key_file:
-#     private_key = serialization.load_pem_private_key(
-#         key_file.read(),
-#         password=None,
-#         backend = default_backend()
-#     )
-
-# python enc.py --d sdfsdf.jsn
-# {
-#     RSAKey: "Asdasdasdas",
-#     ciphertext: "asdouh319r8gefbweof",
-# }
