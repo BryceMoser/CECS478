523798e0ac0821c01c5a526f4c4995c9e04fbc71
FryedBryce <brycemoser@yahoo.com>
2018-10-11 18:01:46 -0700

    Obtain Info from Json?

--- E2EChat/Decryption.py
+++ E2EChat/Decryption.py
@@ -1,12 +1,19 @@
+import sys
+import json
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 
+#Obtaining information about JSON from command line
+if '--d' in sys.argv and 'RSAcipher:' in sys.argv:
+    RSAciphertxt = sys.argv[sys.argv.index('RSAcipher:')+1]
+    AESciphertxt = sys.argv[sys.argv.index('AEScipher:')+1]
 
-##retrieving private key
-with open("RSA2048 KeyPair\privkey.ppk", "rb") as key_file:
-    private_key = serialization.load_pem_private_key(
-        key_file.read(),
-        password=None,
-        backend = default_backend()
-    )
+
+#Retrieving private key
+# with open("RSA2048 KeyPair\privkey.ppk", "rb") as key_file:
+#     private_key = serialization.load_pem_private_key(
+#         key_file.read(),
+#         password=None,
+#         backend = default_backend()
+#     )
