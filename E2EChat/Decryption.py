3a672ffbe80b5354eff201d42b6aaa55c3aab2a2
FryedBryce <brycemoser@yahoo.com>
2018-10-14 19:58:17 -0700

    Serialization

--- E2EChat/Decryption.py
+++ E2EChat/Decryption.py
@@ -4,13 +4,23 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 
+#Generates an RSA object with the loaded .pem private key
+with open('RSA2048 KeyPair\privkey.ppk') as key_file:
+    prvK = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend=default_backend
+    )
+
 #Parsing information about JSON from command line:
 if '--d' in sys.argv:
     with open(sys.argv[sys.argv.index('--d')+1]) as enc:
         json_data = json.loads(enc)
-        #Assuming the title of json is "Message"
         for p in json_data['Message']:
-            print('RSA Ciphertext'+p['rsa_ciphertxt'])
-            print('AES Ciphertext'+p['aes_ciphertxt'])
-            print('HMAC tag'+p['htag'])
-            print('Key path'+p['path'])
+            rsaCipher = p['rsa_ciphertext']
+
+
+    # print('RSA Ciphertext'+p['rsa_ciphertxt'])
+    # print('AES Ciphertext'+p['aes_ciphertxt'])
+    # print('HMAC tag'+p['htag'])
+    # print('Key path'+p['path'])
