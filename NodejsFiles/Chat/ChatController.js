70880774d8b18c14f477635494a051559ffe2d63
ChrisMeyer7088 <chris_meyer_2@hotmail.com>
2018-11-05 21:22:16 -0800

    Phase IV complete

--- NodejsFiles/Chat/ChatController.js
+++ NodejsFiles/Chat/ChatController.js
@@ -4,22 +4,32 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({extended : true}));
 router.use(bodyParser.json());
 var Chat = require('./Chat.js');
-var VerifyToken = require('./VerifyToken');
+var VerifyToken = require('../Authorization/VerifyToken');
 
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
 
-router.post('message', VerifyToken, function(req, res, next) {
-    if (err) return res.status(500).send('Error on the server.');
+router.post('/', VerifyToken, function(req, res, next) {
     Chat.create({
         email: req.body.email,
         message: req.body.message
     },
-    function(err, user){
+    function(err){
         if(!err) res.status(500).send("Error sending the message");
     });
     res.status(200).send({message: req.body.message});
+});
+
+router.get('/', VerifyToken, function(req, res) {
+    Chat.find({email: req.headers['email']},
+        (err, messages) => {
+        if(err) return res.status(500).send("Could not locate any messages");
+        console.log(messages);
+        res.status(200).send(messages);
+        Chat.find({email: req.headers['email']}).remove().exec();
+    });
+    
 });
 
 module.exports = router;
